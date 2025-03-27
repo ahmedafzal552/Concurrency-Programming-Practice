c5695c9079a508b8d7f94c657519b032e68ff7d1
ahmedafzal <2022552@student.cct.ie>
2025-03-27 14:09:19 +0000

    Form Parsing

--- index.js
+++ index.js
@@ -19,8 +19,22 @@ app.get("/html", (req, res) => {
     res.sendFile("index.html", {root: __dirname});
 })
 
+//url parameter
+app.get("/params/:display", (req, res) => {
+    res.send(req.params.display);
+})
 
+//url querey
+app.get("/query", (req, res) => {
+    res.send(`The number is: ${req.query.number} and the text is: ${req.query.text}`);
+})
 
+app.post("/form", (req, res) => {
+    if (req.body.password == "password") {
+        res.redirect("/hello");
+    }
+    res.send(`The username is: ${req.body.username} and the password is: ${req.body.password}.`);
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
